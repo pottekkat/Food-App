4841cde2b9d952c7e6fa6a189b9a078f15e04c52
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-01-16 22:51:08 +0530

    First run successful- next step styling

--- lib/categoriesScreen.dart
+++ lib/categoriesScreen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
+import './dummyData.dart';
+import './categoryItem.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(children: <Widget>[], gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3/2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Food App'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((cat) => CategoryItem(
+                  cat.title,
+                  cat.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
