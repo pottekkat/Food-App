5c1221f7254320d402d26c3ee004c1fc8d1cf022
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-01-17 10:19:20 +0530

    updated with new fonts

--- lib/categoriesScreen.dart
+++ lib/categoriesScreen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Food App'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((cat) => CategoryItem(
                   cat.title,
