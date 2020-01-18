96dd04820be1b7432c64a933c6fb691f79caa1aa
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-01-18 23:26:22 +0530

    updated meal detail page added the ingredients to a scrollable list

--- lib/screens/mealDetailScreen.dart
+++ lib/screens/mealDetailScreen.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
+import '../dummyData.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
+
+  Widget buildSectionTitle(BuildContext context, String title){
+    return Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.title,
+            ),
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text('$mealId'),),
-      body: Center(
-        child: Text('Meal: $mealId'),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          buildSectionTitle(context, 'Ingredients'),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).accentColor,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 10,
+                  ),
+                  child: Text(selectedMeal.ingredients[index]),
+                ),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          ),
+          buildSectionTitle(context, 'Steps'),
+        ],
       ),
     );
   }
